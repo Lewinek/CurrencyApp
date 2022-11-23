358df1c86ae5d0d01a821eb8af39237fc89bf705
Krzysztof Lewiński <krzysztof@coderspeak.com>
2022-11-23 22:10:03 +0100

    Add required BaseActivity implementations

--- app/src/main/java/com/example/currencyapp/MainActivity.kt
+++ app/src/main/java/com/example/currencyapp/MainActivity.kt
@@ -1,4 +1,11 @@
 package com.example.currencyapp
 
-class MainActivity {
+import com.example.core_architecture.BaseActivity
+import org.koin.androidx.viewmodel.ext.android.viewModel
+
+class MainActivity :
+    BaseActivity<MainActivityUiModel, MainActivityViewModel>(R.layout.activity_main) {
+    override val viewModel: MainActivityViewModel by viewModel()
+
+    override fun render(model: MainActivityUiModel) {}
 }
