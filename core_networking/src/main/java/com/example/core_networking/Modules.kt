240c2aaaa8bad71a08b695f85a0c0ab9420a245d
Krzysztof Lewiński <krzysztof@coderspeak.com>
2022-11-25 22:15:10 +0100

    Add Retrofit Builder with baseUrl

--- core_networking/src/main/java/com/example/core_networking/Modules.kt
+++ core_networking/src/main/java/com/example/core_networking/Modules.kt
@@ -1,7 +1,13 @@
 package com.example.core_networking
 
 import org.koin.dsl.module
+import retrofit2.Retrofit
 
 val networkingModule = module {
 
+    single {
+        Retrofit.Builder()
+            .baseUrl("https://hiring.revolut.codes/api/android")
+            .build()
+    }
 }
