f12b56e7f023b49506d8d29576c0aecfbe4a1b4b
Krzysztof Lewiński <krzysztof@coderspeak.com>
2022-11-26 21:59:21 +0100

    Add getting rates endpoint

--- core_networking/src/main/java/com/example/core_networking/CurrencyApi.kt
+++ core_networking/src/main/java/com/example/core_networking/CurrencyApi.kt
@@ -1,4 +1,10 @@
 package com.example.core_networking
 
+import retrofit2.http.GET
+import retrofit2.http.Query
+
 interface CurrencyApi {
+
+    @GET("latest")
+    fun getRatesByBaseCurrency(@Query("base") base: String): CurrencyResponse
 }
