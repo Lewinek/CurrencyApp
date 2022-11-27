8d94398af197181ed6f91c73af15f18be6e5d244
Krzysztof Lewiński <krzysztof@coderspeak.com>
2022-11-27 19:19:07 +0100

    Add navigation action

--- app/src/main/java/com/example/currencyapp/MainActivity.kt
+++ app/src/main/java/com/example/currencyapp/MainActivity.kt
@@ -1,11 +1,18 @@
 package com.example.currencyapp
 
+import android.os.Bundle
+import androidx.navigation.findNavController
 import com.example.core_architecture.BaseActivity
 import org.koin.androidx.viewmodel.ext.android.viewModel
 
 class MainActivity :
     BaseActivity<MainActivityUiModel, MainActivityViewModel>(R.layout.activity_main) {
     override val viewModel: MainActivityViewModel by viewModel()
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        findNavController(R.id.navHostFragment).navigate(R.id.actionNavigateToRate)
+    }
 
     override fun render(model: MainActivityUiModel) {}
 }
