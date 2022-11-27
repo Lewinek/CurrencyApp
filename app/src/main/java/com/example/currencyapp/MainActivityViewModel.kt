3daa06eec58670b43b99f4ad4371b6594ef34459
Krzysztof Lewiński <krzysztof@coderspeak.com>
2022-11-27 11:23:55 +0100

    Add getting rates on MainActivityViewModel init

--- app/src/main/java/com/example/currencyapp/MainActivityViewModel.kt
+++ app/src/main/java/com/example/currencyapp/MainActivityViewModel.kt
@@ -1,6 +1,21 @@
 package com.example.currencyapp
 
+import androidx.lifecycle.viewModelScope
 import com.example.core_architecture.BaseViewModel
+import com.example.core_networking.CurrencyRepository
+import kotlinx.coroutines.launch
 
-class MainActivityViewModel : BaseViewModel<MainActivityUiModel>() {
+class MainActivityViewModel(private val repository: CurrencyRepository) :
+    BaseViewModel<MainActivityUiModel>() {
+
+    init {
+        getRates()
+    }
+
+    private fun getRates() {
+        viewModelScope.launch {
+            val rates = repository.getRatesByBaseCurrency()
+        }
+
+    }
 }
