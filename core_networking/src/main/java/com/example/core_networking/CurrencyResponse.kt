daebde1ef3847737da1212c3e518a113851edc1d
Krzysztof Lewiński <krzysztof@coderspeak.com>
2022-11-27 19:05:47 +0100

    Convert rates map to list

--- core_networking/src/main/java/com/example/core_networking/CurrencyResponse.kt
+++ core_networking/src/main/java/com/example/core_networking/CurrencyResponse.kt
@@ -2,5 +2,7 @@ package com.example.core_networking
 
 data class CurrencyResponse(
     val baseCurrency: String,
-    val rates: Rates
-)
+    val rates: Map<String, Double>
+) {
+   val convertedRates: List<Rate> = rates.map { Rate(it.key, it.value) }
+}
