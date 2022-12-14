9b71d40c9e0d17fa331876436fe607cc8c8c9483
Krzysztof Lewiński <krzysztof@coderspeak.com>
2022-12-14 21:13:42 +0100

    Add test that checks that the data has been set on successful response

--- app/src/test/java/com/example/currencyapp/rate/RateViewModelTest.kt
+++ app/src/test/java/com/example/currencyapp/rate/RateViewModelTest.kt
@@ -4,7 +4,7 @@ import com.example.core_networking.Currency
 import com.example.core_networking.CurrencyRepositoryImpl
 import com.example.core_networking.ResultWrapper
 import com.example.currencyapp.ViewModelTest
-import com.google.common.truth.Truth
+import io.kotest.matchers.shouldBe
 import io.mockk.coEvery
 import io.mockk.mockk
 import org.jetbrains.annotations.TestOnly
@@ -12,21 +12,36 @@ import org.junit.jupiter.api.Test
 
 internal class RateViewModelTest : ViewModelTest() {
     @Test
-    fun `on start it should load data `() {
-        val expectedList = listOf(Currency.mock(), Currency.mock(), Currency.mock())
+    fun `should set result in live data on successful response`() {
+        //Prepare
+        val givenList = listOf(
+            Currency.mock(name = "BRL"),
+            Currency.mock(name = "EUR"),
+            Currency.mock(name = "PLN")
+        )
+        val expectedList = listOf(
+            Currency.mock(name = "GBP", isBaseCurrency = true),
+            Currency.mock(name = "BRL"),
+            Currency.mock(name = "EUR"),
+            Currency.mock(name = "PLN")
+        )
         val repository = mockk<CurrencyRepositoryImpl> {
-            coEvery { getRatesByBaseCurrency("") } returns ResultWrapper.Success(expectedList)
+            coEvery { getRatesByBaseCurrency("GBP") } returns ResultWrapper.Success(givenList)
         }
         val viewModel = RateViewModel(repository)
-        val response = viewModel.getRatesByBaseCurrency("")
-        Truth.assertThat(response is ResultWrapper.Success).isTrue()
+
+        //When
+        viewModel.getRatesByBaseCurrency("GBP")
+
+        //Then
+        viewModel.uiLiveData.value?.rates shouldBe expectedList
     }
 }
 
 @TestOnly
-fun Currency.Companion.mock() = Currency(
-    name = "EUR",
+fun Currency.Companion.mock(name: String = "GBP", isBaseCurrency: Boolean = false) = Currency(
+    name = name,
     value = 1.toBigDecimal(),
-    isBaseCurrency = false,
+    isBaseCurrency = isBaseCurrency,
     convertedValue = 1.toBigDecimal()
 )
